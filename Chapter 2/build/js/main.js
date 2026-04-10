aa83567c89113165bc0ae1d70d5efbd79853a5d3
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2026-04-10 11:00:29 +0000

    Chapter 2 completed

--- Chapter 2/build/js/main.js
+++ Chapter 2/build/js/main.js
@@ -1,2 +1,19 @@
+let userName;
+let emailID;
+let UID;
+let phoneNo;
+let isPassed;
+let rollNo;
+userName = "Adrishikhar Chowdhury";
+emailID = "ec24.adrishikhar.chowdhury@stcet.ac.in";
+UID = "3bf82032-fcff-4f25-bc86-96e68cefa1c4";
+phoneNo = "1234567890";
+isPassed = true;
+rollNo = 9;
+console.log(`Name: ${userName}`);
+console.log(`Email: ${emailID}`);
+console.log(`Roll No. ${rollNo}`);
+console.log(`Phone No. ${phoneNo}`);
+console.log(`Remarks: ${isPassed ? "Yes" : "No"}`);
 export {};
 //# sourceMappingURL=main.js.map
