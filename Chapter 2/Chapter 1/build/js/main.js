55b5de94182857accfafd40c312d6bcc8e9ead9d
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2026-04-10 10:35:02 +0000

    Chapter 1 and 2 Added

---
+++ Chapter 2/Chapter 1/build/js/main.js
@@ -0,0 +1,10 @@
+let username = "Shibaji";
+console.log(username);
+let a = 69;
+let b = '420';
+let c = 'Arjun ekta obhodro chele';
+let d = 67;
+console.log(a * d);
+console.log(b + " " + c);
+export {};
+//# sourceMappingURL=main.js.map
